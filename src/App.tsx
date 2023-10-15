d4245277bd94ee16435b19112aa4a20b95e3c4a1
Matt Lipski <matt.lipski@sprint-education.co.uk>
2023-10-15 15:35:55 +0100

    Pretty much working

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import React, { useEffect, useState } from "react";
 import { LevenshteinDistance } from "./Lev";
 import { GuessStoreHelper } from "./Helpers/GuessStore";
 import { GameHelper } from "./Helpers/GameHelper";
-import { IconButton, Tooltip } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SendIcon from "@mui/icons-material/Send";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Button from "@mui/material/Button";
 
 function App() {
   const [currentInput, setCurrentInput] = useState<string>("");
   const [lastWord, setLastWord] = useState<string>("");
   const [distance, setDistance] = useState<number>();
+  const [winState, setWinState] = useState<boolean>(false);
+
+  const [hint, setHint] = useState<string>("");
+  const [showHint, setShowHint] = useState<boolean>(false);
 
   const [alreadyGuessed, setAlreadyGuessed] = useState<string>("");
 
@@ -27,12 +41,19 @@ function App() {
       distance: currentDistance.distance,
     };
 
+    if (currentDistance.distance === 0) {
+      setWinState(true);
+    } else {
+      setWinState(false);
+    }
+
     const alreadyGuessed = guesses.find(
       (o) => o.value.toLowerCase() == guess.value.toLowerCase()
     );
 
     setDistance(guess.distance);
     setLastWord(guess.value);
+    setCurrentInput("");
 
     if (!alreadyGuessed) {
       setAlreadyGuessed("");
@@ -45,90 +66,131 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    if (lastWord.length < todaysWord.length) {
+      setHint("You are missing some letters");
+    }
+    if (lastWord.length > todaysWord.length) {
+      setHint("Your word has too many letters");
+    }
+    if (lastWord.length == todaysWord.length) {
+      setHint(
+        `Your word is the correct length but ${distance} letters are incorrect`
+      );
+    }
+  }, [lastWord]);
+
   return (
     <div className="App">
       <h1>Some word game</h1>
-      <form
-        className="word-input"
-        onSubmit={(ev) => {
-          ev.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <input
-          className="guess-input"
-          type="text"
-          pattern="[a-zA-Z]+"
-          required
-          onChange={(ev) => {
-            setCurrentInput(ev.target.value);
+
+      <div className="main-wrapper">
+        <form
+          className="word-input"
+          onSubmit={(ev) => {
+            ev.preventDefault();
+            handleSubmit();
           }}
-        />
-        <button className="guess-button" type="submit">
-          Guess
-        </button>
-      </form>
+        >
+          <input
+            className="guess-input"
+            type="text"
+            pattern="[a-zA-Z]+"
+            required
+            value={currentInput}
+            onChange={(ev) => {
+              setCurrentInput(ev.target.value);
+            }}
+          />
 
-      <div className="current-container">
-        {lastWord && (
-          <>
-            <span className="last-word">{lastWord}</span> is{" "}
-            <span className="distance">{distance}</span> operations away from
-            the answer.
-            <Tooltip
-              title="An operation can be either adding, removing or replacing a letter
+          <Button
+            variant="contained"
+            disableElevation
+            className="guess-button"
+            type="submit"
+          >
+            <SendIcon />
+          </Button>
+        </form>
+
+        <div className="current-container">
+          {lastWord && winState && (
+            <>
+              <span className="last-word">{lastWord}</span> is the answer!
+            </>
+          )}
+          {lastWord && !winState && (
+            <>
+              <span className="last-word">{lastWord}</span> is{" "}
+              <span className="distance">{distance}</span> operations away from
+              the answer.
+              <Tooltip
+                title="An operation can be either adding, removing or replacing a letter
               in the word."
-            >
-              <IconButton>
-                <HelpOutlineIcon />
-              </IconButton>
-            </Tooltip>
-          </>
-        )}
-      </div>
+              >
+                <IconButton>
+                  <HelpOutlineIcon />
+                </IconButton>
+              </Tooltip>
+            </>
+          )}
+        </div>
 
-      <div className="guess-container">
-        {guesses.length > 0 && (
-          <>
-            <h3>Your guesses today:</h3>
-            <ul>
-              {guesses
-                .sort((a, b) => a.distance - b.distance)
-                .map((guess) => {
-                  return (
-                    <li
-                      className={`guess ${
-                        alreadyGuessed.toLowerCase() ==
-                        guess.value.toLowerCase()
-                          ? " already-guessed"
-                          : ""
-                      } ${
-                        lastWord.toLowerCase() == guess.value.toLowerCase()
-                          ? " last-guess"
-                          : ""
-                      }`}
-                    >
-                      <div className={`guess-value`}>{guess.value}</div>
-                      <div className="guess-distance">{guess.distance}</div>
-                    </li>
-                  );
-                })}
-            </ul>
-          </>
-        )}
+        <div className="guess-container">
+          {guesses.length > 0 && (
+            <>
+              <h3>Your Guesses</h3>
+              <ul>
+                {guesses
+                  .sort((a, b) => a.distance - b.distance)
+                  .map((guess) => {
+                    return (
+                      <li
+                        key={guess.value}
+                        className={`guess ${
+                          alreadyGuessed.toLowerCase() ==
+                          guess.value.toLowerCase()
+                            ? " already-guessed"
+                            : ""
+                        } ${
+                          lastWord.toLowerCase() == guess.value.toLowerCase()
+                            ? " last-guess"
+                            : ""
+                        }`}
+                      >
+                        <div className={`guess-value`}>{guess.value}</div>
+                        <div className="guess-distance">
+                          {guess.distance == 0 ? "WINNER" : guess.distance}
+                        </div>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </>
+          )}
+        </div>
       </div>
 
       <div className="footer">
-        <div className="game-id">Game ID: {gameId}</div>
-        <div className="game-options">
-          <button
+        <div className="footer-info">Game ID: {gameId}</div>
+        <div className="footer-options">
+          <Button
+            variant="contained"
             onClick={() => {
               GuessStoreHelper.clearAll();
               setGuesses([]);
             }}
           >
             Clear Guesses
-          </button>
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setShowHint(!showHint);
+            }}
+          >
+            {showHint ? hint : "Show Hint"}
+          </Button>
         </div>
       </div>
     </div>
--- src/Lev.ts
+++ src/Lev.ts
@@ -19,34 +19,32 @@ export const LevenshteinDistance = (
 
   // Create base matrix
   const levMatrix: Array<Array<number>> = [];
-  for (let i = 0; i < sourceSplit.length; i++) {
+  for (let i = 0; i <= sourceSplit.length; i++) {
     levMatrix[i] = [];
-    for (let j = 0; j < targetSplit.length; j++) {
+    for (let j = 0; j <= targetSplit.length; j++) {
       levMatrix[i][j] = 0;
     }
   }
 
-  for (let i = 1; i < sourceSplit.length; i++) {
+  for (let i = 1; i <= sourceSplit.length; i++) {
     levMatrix[i][0] = i;
   }
 
-  for (let j = 1; j < targetSplit.length; j++) {
+  for (let j = 1; j <= targetSplit.length; j++) {
     levMatrix[0][j] = j;
   }
 
   // Calculate distance
-  for (let j = 1; j < targetSplit.length; j++) {
-    for (let i = 1; i < sourceSplit.length; i++) {
-      let subCost = 0;
-      if (sourceSplit[i] !== targetSplit[j]) {
-        subCost = 1;
+  for (let j = 1; j <= targetSplit.length; j++) {
+    for (let i = 1; i <= sourceSplit.length; i++) {
+      let subCost = 1;
+      if (sourceSplit[i - 1] == targetSplit[j - 1]) {
+        subCost = 0;
       }
 
       const removeCost = levMatrix[i - 1][j] + 1;
       const insertCost = levMatrix[i][j - 1] + 1;
       const totalSubCost = levMatrix[i - 1][j - 1] + subCost;
-      console.log(removeCost, insertCost, totalSubCost);
-
       const cost = Math.min(removeCost, insertCost, totalSubCost);
 
       switch (cost) {
@@ -65,11 +63,9 @@ export const LevenshteinDistance = (
     }
   }
 
-  console.log(levMatrix);
-
   // Last entry in the matrix is the distance
   return {
-    distance: levMatrix[sourceSplit.length - 1][targetSplit.length - 1],
+    distance: levMatrix[sourceSplit.length][targetSplit.length],
     removals: removals,
     additions: additions,
     substitutions: substitutions,
